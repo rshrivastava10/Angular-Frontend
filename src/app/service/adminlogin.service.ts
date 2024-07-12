b762e4a797bd7db8f9de9a4af8091945a161af83
Rishika Shrivastava <100477384+rshrivastava10@users.noreply.github.com>
2024-07-12 23:44:18 +0530

    Update adminlogin.service.ts

--- src/app/service/adminlogin.service.ts
+++ src/app/service/adminlogin.service.ts
@@ -7,8 +7,9 @@ import {JwtHelperService} from  '@auth0/angular-jwt';
   providedIn: 'root'
 })
 export class AdminloginService {
-  apiUrl:string='http://localhost:56577/api/Login';
-  imageUrl:string='http://localhost:56577';
+  //Added api/Login
+  apiUrl:string='http://localhost:5140/api/Login';  
+  imageUrl:string='http://localhost:5140';
 
   currentUser : BehaviorSubject<any> = new BehaviorSubject(null);
   currentUserName : BehaviorSubject<any> = new BehaviorSubject(null);
